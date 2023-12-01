43ba08ce98f14d3b5df2b93d045e13ebd7ada0dd
Shruti Verma <shrutive1009@gmail.com>
2023-12-01 20:55:07 +0530

    loop and condition in js

---
+++ Task2.js
@@ -0,0 +1,11 @@
+const marks = 99;
+
+if (marks > 90) {
+  console.log("Your Grade is A");
+} else if (marks > 70 && marks <= 90) {
+  console.log("Your Grade is B");
+} else if (marks > 50 && marks <= 70) {
+  console.log("Your Grade is C");
+} else {
+  console.log("Your Grade is F");
+}
---
+++ Task5.js
@@ -0,0 +1,10 @@
+let num = 1;
+while (num <= 25) {
+  console.log(num);
+  num++;
+}
+let num2 = 25;
+while (num2 >= 1) {
+  console.log(num2);
+  num2--;
+}
